0798bb4626d3f46223afe22a728bb8fcd264afa4
Michael Munoz <mmunoz00@byui.edu>
2024-02-10 20:03:35 -0700

    Comments Added

--- Memory Match/myScript.js
+++ Memory Match/myScript.js
@@ -4,43 +4,25 @@ let firstCard, secondCard;
 let lockBoard = false;
 let score = 0;
 
+// Displays current score of 0 at start of the game
 document.querySelector(".score").textContent = score;
 
-// fetch("./data/cards.json")
-//     .then((res) => res.json());
-//     .then((data) => {
-//         cards = [...data, ...data];
-//         shuffleCards();
-//         generateCards();
-//     });
-
+// Creates two of each type of card and assigns a name and image to each type
 for (let index = 1; index <= 9; index++) {
-
     cards.push({"name": index, "image": "images/cardicons/icon" + index + ".png"});
     cards.push({"name": index, "image": "images/cardicons/icon" + index + ".png"});
 }
 
+// Randomizes the order of the cards and assigns images
 shuffleCards();
 generateCards();
 
-
-// function shuffleCards() {
-//     let index = cards.length,
-//     rand,
-//     temp;
-//     while (index !== 0) {
-//         rand = Math.floor(Math.random() * index);
-//         index -= 1;
-//         temp = cards[index];
-//         cards[index] = cards[rand];
-//         cards[index] = temp;
-//     }
-// }
-
+// Randomizes the cards order by shuffling the index of the list items
 function shuffleCards() {
     cards = cards.sort(() => Math.random() - 0.5);
 }
 
+// Generates the HTML element that contains each card's class and image
 function generateCards() {
     for (let card of cards) {
         const cardElement = document.createElement("div");
@@ -57,6 +39,8 @@ function generateCards() {
     }
 }
 
+// Checks if card can be flipped and flips if passes all checks, then checks if card is first
+// or second flipped, and determines if two cards flipped is a match
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
@@ -75,6 +59,8 @@ function flipCard() {
     checkMatch();
 }
 
+// Checks the name of the cards against each other and unflips the cards if there is no match and subtracts 1 point, 
+// or locks the cards facing forward if it is a match and adds 10 points.
 function checkMatch() {
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
 
@@ -91,6 +77,7 @@ function checkMatch() {
     }
 }
 
+// Locks the cards from being clicked again if currently flipped then resets for user input
 function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
@@ -98,6 +85,7 @@ function disableCards() {
     resetBoard();
 }
 
+// flips both chosen cards back over after short delay
 function unflipCards() {
     setTimeout(() => {
         firstCard.classList.remove("flipped");
@@ -106,6 +94,8 @@ function unflipCards() {
     }, 1000);
 }
 
+// sets chosen cards to null so user can choose two new cards, with short delay to prevent user from
+// spam flipping cards
 function resetBoard() {
     firstCard = null;
     secondCard = null;
@@ -114,6 +104,7 @@ function resetBoard() {
     }, 500);
 }
 
+// resets game to start, and reloads the application
 function restart() {
     resetBoard();
     shuffleCards();
